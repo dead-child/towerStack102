b2107cea4dced49aabd605b1e3ac32c7d79262b5
dead-child <77417186+dead-child@users.noreply.github.com>
2021-07-15 14:23:18 +1200

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,50 @@
+class Block{
+  constructor(x, y, width, height) {
+    var options = {
+      'restitution' :0.4,
+        'friction' :0.0,
+    }
+  //  super(50,50),
+      this.Visibility= 255;
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      this.image = loadImage("block.png");
+      World.add(world, this.body);
+
+    }
+ 
+
+    display(){
+     // var pos= this.body.position;
+    //  rectMode(CENTER);
+      //rect(pos.x,pos.y,this.width, this.height);
+
+console.log(this.body.speed)  
+var pos = this.body.position;
+
+
+      if(this.body.speed < 3){
+        imageMode(CENTER);
+        image(this.image,this.body.position.x, this.body.position.y, 50,50);
+
+
+    //    super.display();
+
+    
+      }
+      else{
+       World.remove(world,this.body);
+        push();
+        
+      this.Visibility = this.Visibility-5;      
+        tint(255,this.Visibility)
+        image(this.image,this.body.position.x, this.body.position.y, 50,50);
+        pop();
+
+
+
+      }
+
+    }
+  }
